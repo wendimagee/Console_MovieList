a1a81d7b50e278f33b83272b7319c061e54a9772
Wendi Magee <wendimagee@gmail.com>
2021-01-28 14:58:07 -0500

    Add project files.

---
+++ MovieList/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieList
+{
+    class Movie
+    {
+        private string Title { get; set; }
+        private string Genre { get; set; }
+
+        public Movie(string Title, string Genre)
+        {
+            this.Title = Title;
+            this.Genre = Genre;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Title: {Title}");
+            Console.WriteLine($"Genre : {Genre}");
+
+        }
+
+    }
+}
---
+++ MovieList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MovieList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool goAgain;
+            while (goAgain)
+            {
+                Console.WriteLine("Hello and Welcome to a very short list of movies!");
+                try
+                {
+
+
+                    Console.WriteLine("Which genre would you like to see? animated, drama, horror, or scifi?");
+                    string input = Console.ReadLine();
+                    string userChoice = input.ToLower();
+                }
+                catch
+                {
+                    Console.WriteLine(@"I'm sorry you must enter ""animated"", ""drama"", ""horror"", or ""scifi""");
+                }
+            }
+        }
+    }
+}
